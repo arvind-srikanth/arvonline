2dc26fa3fee1fde7dc1f43f21fb22febf1c2c96e
arv <152562862+arvind-srikanth@users.noreply.github.com>
2024-05-12 15:55:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -65,28 +65,3 @@ $(document).ready(function(){
       });
   });
   
-  document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll(".gallery-image");
-
-    images.forEach(function(image) {
-        image.addEventListener("click", function() {
-            const lightbox = document.createElement("div");
-            lightbox.classList.add("lightbox");
-
-            const imageClone = image.cloneNode(true);
-            imageClone.classList.remove("gallery-image"); // Remove class to avoid hover effect
-            lightbox.appendChild(imageClone);
-
-            const caption = document.createElement("div");
-            caption.classList.add("lightbox-caption");
-            caption.textContent = image.dataset.caption;
-            lightbox.appendChild(caption);
-
-            document.body.appendChild(lightbox);
-
-            lightbox.addEventListener("click", function() {
-                document.body.removeChild(lightbox);
-            });
-        });
-    });
-});
